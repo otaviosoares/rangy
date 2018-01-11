8b26d282da6d334dbc871dadc2375fffea629c36
Otavio Augusto <otaviokk@gmail.com>
2018-01-11 19:08:00 -0200

    fix forceExclusive

--- src/modules/rangy-highlighter.js
+++ src/modules/rangy-highlighter.js
@@ -378,7 +378,7 @@ rangy.createModule("Highlighter", ["ClassApplier"], function(api, module) {
                         //  2. partially or entirely erased (className === null)
                         //  3. partially or entirely replaced (isSameClassApplier == false && exclusive == true)
                         if (    (highlightCharRange.intersects(charRange) || highlightCharRange.isContiguousWith(charRange)) &&
-                                (isSameClassApplier || splitHighlight) ) {
+                                (forceExclusive || isSameClassApplier || splitHighlight) ) {
 
                             // Remove existing highlights, keeping the unselected parts
                             if (splitHighlight) {
@@ -387,7 +387,7 @@ rangy.createModule("Highlighter", ["ClassApplier"], function(api, module) {
                                 });
                             }
 
-                            removeHighlight = true;
+                            removeHighlight = !forceExclusive;
                             if (isSameClassApplier && !forceExclusive) {
                                 charRange = highlightCharRange.union(charRange);
                             }
